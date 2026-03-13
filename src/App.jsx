df0a0af69ca17a63c642dd44a111a30d5588c905
Jason Christian <jasonchristian1234@gmail.com>
2026-03-13 22:05:53 +1300

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
 
 const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
-            <h1 className="text-2xl text-gray_gradient underline">Hello, Three.js</h1>
+            <Navbar/>
         </main>
     )
 }
