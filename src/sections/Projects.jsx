13e789da2cda956a8acea7f92a6e9c50589523d6
Jason Christian <jasonchristian1234@gmail.com>
2026-03-21 01:49:15 +1300

    feat: add previous and next button

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {myProjects} from "../constan/index.js";
 
 const Projects = () => {
-    const currentProject = myProjects[0];
+    const projectCount = myProjects.length;
+
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
+
+    const currentProject = myProjects[selectedProjectIndex];
+
+    const handleNavigation = (direction) => {
+        setSelectedProjectIndex((prevIndex) => {
+            if (direction === 'previous') {
+                return prevIndex === 0 ? projectCount - 1 : prevIndex - 1;
+            } else {
+                return prevIndex === projectCount - 1 ? 0 : prevIndex + 1;
+            }
+        })
+    }
 
     return (
         <section className={"c-space my-20"}>
@@ -38,6 +52,16 @@ const Projects = () => {
                             <p>Check Site</p>
                             <img src="/assets/arrow-up.png" className={"w-3 h-3"} alt="arrow"/>
                         </a>
+                    </div>
+
+                    <div className={"flex justify-between items-center mt-7"}>
+                        <button className={"arrow-btn"} onClick={() => handleNavigation('previous')}>
+                            <img src={"/assets/left-arrow.png"} alt={"left arrow"} className={"w-4 h-4"}/>
+                        </button>
+                        <button className={"arrow-btn"} onClick={() => handleNavigation('next')}>
+                            <img src={"/assets/right-arrow.png"} alt={"right arrow"} className={"w-4 h-4"}/>
+                        </button>
+
                     </div>
                 </div>
             </div>
