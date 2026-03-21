d0d1ef39baaec23978a278fc6078366c30e3f4f4
Jason Christian <jasonchristian1234@gmail.com>
2026-03-22 08:19:10 +1300

    feat: add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
+import Contact from "./sections/Contact.jsx";
 
 const App = () => {
     return (
@@ -12,7 +13,8 @@ const App = () => {
             <Hero/>
             <About/>
             <Projects/>
-            <Clients></Clients>
+            <Clients/>
+            <Contact/>
         </main>
     )
 }
